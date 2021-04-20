663a6eb19cb4482ea457f3473ae70903dadecd1b
Keith Rozario <krozario@amazon.com>
2021-04-20 10:37:42 +0800

    with logger

--- functions/goodbye.py
+++ functions/goodbye.py
@@ -1,6 +1,10 @@
 import common
+from aws_lambda_powertools import Logger
 
+logger = Logger()
 
+@logger.inject_lambda_context
 def main(event, context):
-
+    logger.info("Writing out Goodbye")
+    logger.info({"Test": "Hello"})
     return common.common_return("Goodbye-sdiuaghdfiaew7rtqewrv")
